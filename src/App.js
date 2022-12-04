48be8669e07c6ee1e3ba9f9a7194978909c9becc
Jeffrey Mei <meijeffrey98@gmail.com>
2022-12-03 22:22:18 -0800

    added particles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
 import * as React from 'react';
+import Particles from "react-tsparticles";
+import particlesConfig from './config/configParticles';
 
 function App() {
+
+    const particlesInit = useCallback(async (engine) => {
+      console.log(engine);
+        await loadFull(engine);
+    }, []);
+
+    const particlesLoaded = useCallback(async (container) => {
+        await console.log(container);
+    }, []);
 
     return (
       <React.Fragment>
         <div className='app'>
-              <h1>Hello World</h1>
+            <div id="welcome-page">
+                <div className="welcome-page" style={{ position: 'relative', overflow: "hidden" }}>
+                  
+                  <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={particlesConfig}
+                  />
+                  <h1>Hello world</h1>
+                </div>
+              </div>
         </div>
         
       </React.Fragment>
