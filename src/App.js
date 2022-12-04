e0ec3bb7fca79b84a27b979584909c8d91941a02
Jeffrey Mei <meijeffrey98@gmail.com>
2022-12-03 22:25:11 -0800

    added no MUI, re-added first page info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import * as React from 'react';
 import Particles from "react-tsparticles";
 import particlesConfig from './config/configParticles';
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
 
 function App() {
 
@@ -26,7 +28,47 @@ function App() {
                     loaded={particlesLoaded}
                     options={particlesConfig}
                   />
-                  <h1>Hello world</h1>
+                  <header className="App-header">
+                    <div style={{display: 'flex'}}>
+                      <div className="Header-description">
+                        <h1>Dating App Data</h1>
+                        <h3>Discover your dating data</h3>
+                        <div class="picture-row">
+                          <img src={require('./images/all_3.png')}></img>
+                        </div>
+                        <p>
+                        <span class="name"><b>Scroll down</b></span> to see how the rest of the online dating community swipes.
+                          <pre></pre>Upload and discover your statistics in online dating. Browse around to find more
+                            online dating analytics.
+                          </p>
+                      </div>
+                    </div>
+                    <div className='bottom-pic'>
+                      <div className='bottom-pic-column'>
+                        <img src={require('./images/online_dating.png')} style={{width: '250px', height:'auto'}}></img>
+                      </div>
+                      <div className='bottom-pic-column'>
+                        <img src={require('./images/right-arrow-icon-114837-11.png')} style={{width: '250px', height:'auto'}}></img>
+                      </div>
+                      <div className='bottom-pic-column'>
+                        <img src={require('./images/data.png')} style={{width: '250px', height:'auto'}}></img>
+                      </div>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <div className="Header-description">
+                      <h3>Where do you rank amongst other dating app users?</h3>
+                      <p>Fill the form below to do a live search and find out.</p>
+                    </div>
+                  </header>
+                  
                 </div>
               </div>
         </div>
